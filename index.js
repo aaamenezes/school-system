341f3b341e453875149b50a951f585f91404de38
André Menezes <1992menezes@gmail.com>
2024-02-04 02:26:19 -0300

    Start server

--- index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const faker_1 = require("@faker-js/faker");
-// or, if desiring a different locale
-// import { fakerDE as faker } from '@faker-js/faker';
-const randomName = faker_1.faker.person.fullName(); // Rowan Nikolaus
-console.log("randomName:", randomName);
-const randomEmail = faker_1.faker.internet.email(); // Kassandra.Haley@erich.biz
-console.log("randomEmail:", randomEmail);
--- index.ts
+++ index.ts
@@ -1,2 +1,20 @@
-// EXPRESS
 // ZOD ou JSON Schema
+
+import express, { Request as Req, Response as Res } from 'express';
+
+const app = express();
+
+const port = 3000;
+
+app.get('/', (req: Req, res: Res) => {
+  const allData = req.body;
+  res.send({
+    message: `Hello world, App rodando na porta ${port}`
+  });
+});
+
+app.listen(port, () => {
+  console.log(`App rodando na porta ${port}`);
+});
+
+// https://expressjs.com/en/starter/basic-routing.html
